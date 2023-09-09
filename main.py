bc83a5cf60d0b638a1f730df2d5e91d28f6a2b53
tknott95 <tknott95@hotmail.com>
2023-09-08 18:06:12 -0600

    Always Online - DISCORD

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ while(True):
   _pos = mouse.get_position()
   if _pos != (100, 100):
     mouse.move(100, 100, absolute=False, duration=0.2)
+    mouse.click('left')
   else:
     mouse.move(400, 400, absolute=False, duration=0.2)
+    mouse.click('left')
+
   time.sleep(2400)
 
