f52329cef25e1843b0284cdb65ceaa964337bb6e
tknott95 <tknott95@hotmail.com>
2023-09-08 18:41:33 -0600

    pyautogui - mouse wanted sudo

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-import mouse
+
 import time
+
+# NOT USING MOUSE
+# import mouse
+# mouse.move(100, 100, absolute=False, duration=0.2) - works without sudo
+# mouse wants sudo access to do things like mouse.click('left') - wants sudo which is bad
+# instead I use pytautogui.
+
+import pyautogui
+pyautogui.click(100, 100)
 
 while(True):
   _pos = mouse.get_position()
   if _pos != (100, 100):
-    mouse.move(100, 100, absolute=False, duration=0.2)
-    mouse.click('left')
+    pyautogui.click(100, 100)
   else:
-    mouse.move(400, 400, absolute=False, duration=0.2)
-    mouse.click('left')
+    pyautogui.click(400, 400)
 
-  time.sleep(2400)
-
+  time.sleep(270)
